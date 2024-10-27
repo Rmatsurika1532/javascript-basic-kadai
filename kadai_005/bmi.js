078158036e33a63932a1acd9aba60beed4e070a1
Rmatsurika1532 <puppy.hime4265@gmail.com>
2024-10-27 21:51:58 +0900

    kadai_005をアップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,14 @@
+// 変数の宣言
+let bodyWeight;
+let height;
+let bodyMassIndex;
+
+// 変数の代入
+bodyWeight = "68";
+height = "1.7";
+bodyMassIndex = bodyWeight / (height * height);
+
+// コンソールへ出力
+console.log(bodyWeight);
+console.log(height);
+console.log(bodyMassIndex);
